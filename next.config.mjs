6a2d1b25ba35bf5ffe2000c270ef5a29fb9f25fd
DylanRigney <84356573+DylanRigney@users.noreply.github.com>
2024-01-22 21:11:26 -0500

    expirimenting with changing github actions to fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+    
+};
 
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
+export default nextConfig;
