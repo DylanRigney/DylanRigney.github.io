f5f85b18bd7e65ce4882abf3e9fdd6d4efc74af7
DylanRigney <84356573+DylanRigney@users.noreply.github.com>
2024-01-22 20:43:23 -0500

    fixed next.config error

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@
 const nextConfig = {
     output: "export",
     basePath: process.env.NODE_ENV === "production" ? "" : undefined,
-    experimental: {
-      appDir: true,
-    },
+
     images: {
       unoptimized: true,
     },
