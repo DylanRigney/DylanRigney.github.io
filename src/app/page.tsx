b54cd6b6ba648d875d2dbd1023c334c9da8f9d5c
DylanRigney <84356573+DylanRigney@users.noreply.github.com>
2024-01-30 11:36:19 -0500

    got it working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
-import Project from "@/components/project";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
@@ -10,8 +10,8 @@ export default function Home() {
       <SectionDivider />
       <About />
       <SectionDivider />
-      <Project></Project>
-      <SectionDivider></SectionDivider>
+      <Projects />
+      <SectionDivider />
     </main>
   );
 }
